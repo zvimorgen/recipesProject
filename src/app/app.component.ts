ff38ac91263c00dd351bd1ad448276d2af4f513f
Zvimorgen <zvimorgen@gmail.com>
2024-04-08 17:56:51 +0300

    recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  // standalone: true
 })
 export class AppComponent {
-  title = 'market-project';
+  title:string="title";
 }
---
+++ src/app/products.ts
@@ -0,0 +1,66 @@
+export interface Product {
+  id: number;
+  name: string;
+  // price: number;
+  description: string;
+  show: boolean;
+}
+//
+// export const products = [
+//   {
+//     id: 1,
+//     name: 'bread',
+//     // price: 799,
+//     description: '',
+//     show: true,
+//   },
+//   {
+//     id: 2,
+//     name: 'Beef',
+//     // price: 699,
+//     description: '',
+//     show: true,
+//   },
+//   {
+//     id: 3,
+//     name: 'Meat',
+//     // price: 299,
+//     description: '',
+//     show: true,
+//   },
+//   {
+//     id: 4,
+//     name: 'Milk',
+//     // price: 900,
+//     description: '',
+//     show: true,
+//   },
+//   {
+//     id: 5,
+//     name: 'Cheese',
+//     // price: 900,
+//     description: ' ',
+//     show: true,
+//   },
+//   {
+//     id: 6,
+//     name: 'Tomato paste',
+//     // price: 900,
+//     description: '',
+//     show: true,
+//   },
+//   {
+//     id: 7,
+//     name: 'Chili sauce',
+//     // price: 900,
+//     description: '',
+//     show: true,
+//   },
+//   {
+//     id: 8,
+//     name: 'Chicken Breast',
+//     // price: 900,
+//     description: '',
+//     show: true,
+//   },
+// ];
